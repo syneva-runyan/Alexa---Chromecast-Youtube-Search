ad11b8926e7fbb926947ee226f639da7bd0aad38
Syneva Runyan <srunyan@sapient.com>
2016-07-17 21:48:21 +0100

    added comments to alexa file

---
+++ alexa/index.js
@@ -0,0 +1,7 @@
+
+// take in search query from alexa request
+// and then pass into youtube search.
+
+var youtubeSearch = require("../youtube_search/searchYoutube.js").searchYoutube;
+
+youtubeSearch('test search');
